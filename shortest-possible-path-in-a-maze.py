e3b2b640d44dd9b6fb0524e0d5b4d19ebef1ddf1
axell-brendow <breno.axel@gmail.com>
2021-11-19 21:58:04 -0300

    Fix "Shortest Possible Path in a Maze"

--- shortest-possible-path-in-a-maze.py
+++ shortest-possible-path-in-a-maze.py
@@ -4,9 +4,15 @@ def min_steps(start, end, board):  # start = (0, 2), end = (1, 3)
 	directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
 	visited = [[False for _ in range(columns)] for _ in range(lines)]
 	def dfs(i, j):  # i = 0, j = 2  |  i = 0, j = 1
-		if i < 0 or i >= lines or j < 0 or j >= columns or board[i][j]: return float('inf')
+		if (
+			i < 0 or i >= lines
+			or j < 0 or j >= columns
+			or visited[i][j]
+			or board[i][j]
+		): return float('inf')
+
 		if i == end[0] and j == end[1]: return 0
-		if visited[i][j]: return float('inf')
+
 		visited[i][j] = True
 		min_steps = float('inf')
 
@@ -14,6 +20,7 @@ def min_steps(start, end, board):  # start = (0, 2), end = (1, 3)
 			steps = dfs(i + direction[0], j + direction[1])
 			if steps != float('inf'):
 				min_steps = min(min_steps, steps + 1)
+		visited[i][j] = False
 		return min_steps
 
 	steps = dfs(start[0], start[1])
@@ -34,3 +41,14 @@ board = [
     [True, True, True, True],
 ]
 assert min_steps((0, 1), (0, 0), board) == 1
+
+board = [
+	[False,	False,	False,	False,	False],
+	[False,	True,	False,	True,	False],
+	[False,	True,	False,	True,	False],
+	[False,	True,	True,	True,	False],
+	[False,	False,	False,	False,	False],
+]
+start = (4, 0)
+dest = (2, 2)
+assert min_steps(start, dest, board) == 8
